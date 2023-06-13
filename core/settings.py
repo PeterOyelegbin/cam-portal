a2e3cb9f4f85fa0222b0dbd128be04564d2dd46a
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-06-13 21:53:05 +0100

    deployment config

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".up.railway.app"]
 
 # Application definition
 
